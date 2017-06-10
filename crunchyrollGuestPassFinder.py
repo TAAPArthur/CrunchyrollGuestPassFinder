0950b70536c594528f2d8331fbcc2f6bb8dd371d
TAAPArthur <taaparthur@gmail.com>
2017-06-10 11:29:21 -0500

    Removed debugging method

--- crunchyrollGuestPassFinder.py
+++ crunchyrollGuestPassFinder.py
@@ -151,6 +151,7 @@ class CrunchyrollGuestPassFinder:
     def saveScreenshot(self,fileName="screenshot.png"):
         #self.output("saving screen shot to ",fileName)
         #self.driver.save_screenshot(fileName)
+        pass
 
     def output(self,*message):
         
